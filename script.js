814dedc9e79b6d1c1f94e4dd80097411a4958b71
Capslockhuh <jas.andrzejewski2008@gmail.com>
2023-08-14 08:58:54 +0200

    Added basic variables and functions needed for the project

---
+++ script.js
@@ -0,0 +1,25 @@
+let firstNum;
+let operator;
+let secondNum;
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract (a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b === 0) {
+        alert("You can\'t divide by 0!");
+        return;
+    } else {
+        return a / b;
+    }
+}
+
